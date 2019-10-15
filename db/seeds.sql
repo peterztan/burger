9aafe71c5c9d808d142d18cce90af04a62a00657
RandomRamen <pt072093@gmail.com>
2019-10-15 02:16:48 -0500

    update to db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,27 @@
 -- Insert rows into table 'TableName'
 INSERT INTO burgers
-( -- columns to insert data into
- burger_name, devoured
-)
+    ( -- columns to insert data into
+    burger_name, devoured
+    )
 VALUES
-( 
- "THE Breakfast Burger", false
-),
-( 
- "Avocado Chicken Panini", false
+(
+        "THE Breakfast Burger", false
 ),
 (
- "HER Favorite Burger", false   
-);
+        "Avocado Chicken Panini", false
+),
+(
+        "HER Favorite Burger", false   
+),
+(
+        "The Krabby Patty", false
+),
+(
+        "Mediterranean Burger", false
+),
+("The Amazon Burger", false),
+("The Lucky Burger", false),
+("The Not-So-Lucky Burger", false),
+("The Empossible Burger™", false),
+("The Frank-Lin™", false)
+;
